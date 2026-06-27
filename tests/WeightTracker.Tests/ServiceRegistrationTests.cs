6234145db8533ec3ff0d0f2f46e8f7cff9a968c5
carlocgc <carlocgc@hotmail.com>
2026-06-27 09:24:35 +0100

    fix: initialize sqlite database on startup

--- tests/WeightTracker.Tests/ServiceRegistrationTests.cs
+++ tests/WeightTracker.Tests/ServiceRegistrationTests.cs
@@ -1,5 +1,11 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using WeightTracker.Web.Data;
 using WeightTracker.Web.Services;
 
 namespace WeightTracker.Tests;
@@ -9,7 +15,18 @@ public sealed class ServiceRegistrationTests
     [Fact]
     public void ConfigureServices_ResolvesEntryServices()
     {
-        using var factory = new WebApplicationFactory<Program>();
+        using var connection = new SqliteConnection("Data Source=:memory:");
+        connection.Open();
+        using var factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureLogging(logging => logging.ClearProviders());
+                builder.ConfigureServices(services =>
+                {
+                    services.RemoveAll<DbContextOptions<WeightTrackerDbContext>>();
+                    services.AddDbContext<WeightTrackerDbContext>(options => options.UseSqlite(connection));
+                });
+            });
         using var scope = factory.Services.CreateScope();
 
         Assert.IsType<SystemClock>(scope.ServiceProvider.GetRequiredService<IClock>());
